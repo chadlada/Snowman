113387dae09bbf71a9bed9468f9ee6a95e755291
chadlada <chadlada@gmail.com>
2022-04-11 20:02:48 -0400

    static HTML implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
 import React from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  return (
+    <main>
+      <header>
+        <h1>Do you want to build a snowman?</h1>
+      </header>
+      <body>
+        <section className="instructions">
+          <strong>Instructions:</strong>jkhkjhjkhkjhkjhjkhkjhkjhkjhk
+        </section>
+        <section className="snowman-image">
+          <p>Placeholder</p>
+        </section>
+        <section className="word-display">_*_*_*_*_*_*_*</section>
+        <section className="alphabet-buttons">
+          <button>a</button>
+          <button>b</button>
+          <button>c</button>
+          <button>d</button>
+          <button>e</button>
+          <button>f</button>
+          <button>g</button>
+          <button>h</button>
+          <button>i</button>
+          <button>j</button>
+          <button>k</button>
+          <button>l</button>
+          <button>m</button>
+          <button>n</button>
+          <button>o</button>
+          <button>p</button>
+          <button>q</button>
+          <button>r</button>
+          <button>s</button>
+          <button>t</button>
+          <button>u</button>
+          <button>v</button>
+          <button>w</button>
+          <button>x</button>
+          <button>y</button>
+          <button>z</button>
+        </section>
+      </body>
+      <footer>Made by Chad Lada</footer>
+    </main>
+  )
 }
